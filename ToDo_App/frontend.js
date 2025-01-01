1d5803aa14254e8db3d09ce01bcb38a4d8e8dc7b
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2025-01-01 19:32:27 +0530

    Create frontend.js

---
+++ ToDo_App/frontend.js
@@ -0,0 +1,17 @@
+// async function addTodo() {
+//     let res = await fetch('http://localhost:3000/todos',{
+//         // http://loocalhost:3000/todos
+//         method : "POST",
+//         body : JSON.stringify({
+//             title: 'Todo',
+//             description: 'from frontend'
+//         }),
+//         headers:{
+//             "Content-Type": "application/json"
+//         }
+//     })
+//     let json= await res.json()
+//     console.log(json);
+// }
+
+// addTodo()
