50c866d55a957b65ec9e2dd5ab1cb089532cfbc6
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2025-01-03 19:10:00 +0530

    create

--- BlogApp/index.js
+++ BlogApp/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+
 app.listen(3000,(req,res)=>{
     console.log('Server is running on port 3000');
 })
@@ -10,7 +11,7 @@ app.use(express.json())
 const blogs=[]
 
 app.post('/blogs',(req,res)=>{
-    blogs.push(req.body)
+    blogs.push({...req.body, id : blogs.length +1})
 
    return res.json({message:"Blog created successfully"})
 
@@ -20,13 +21,26 @@ app.get('/blogs',(req,res)=>{
     return res.json({...publicBlogs})
     
 })
+
 app.get('/blogs/:id',(req,res)=>{
+    const {id}=req.params
+    let searchBlogs=blogs.filter(blog=>blog.id == id)
+    return res.json({searchBlogs})
     
 })
 
 app.patch('/blogs/:id',(req,res)=>{
-
+    const {id}=req.params
+    let updatedBlog=blogs.findIndex(blog=>blog.id == id)
+    blogs[updatedBlog]={...blogs[updatedBlog],...req.body}
+    // let updatedBlog=blogs.map((blog,index) => blog.id == id ? ({...blogs[index],...req.body}) : blogs)
+    // blogs=[...updatedBlog]
+    return res.json({message : "Blog updated successfully"})
 })
 app.delete('/blogs/:id',(req,res)=>{
+    const {id}=req.params
+    let blogIndex=blogs.findIndex(blog=>blog.id == id)
+    blogs.splice(blogIndex,1)
+    return res.json({message : "Blog deleted successfully"})
 
 })
