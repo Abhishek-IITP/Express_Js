002700f47599d29518e568c7c03f1db6485ead3c
Abhishek-IITP <abhishekmohanty7325@gmail.com>
2025-02-02 10:52:55 +0530

    new commit

--- BlogApp_users/Backend/Middlewares/auth.js
+++ BlogApp_users/Backend/Middlewares/auth.js
@@ -1,4 +1,37 @@
-const verifyUser= async()=>{
+const { verifyJWT } = require("../Utils/generateTokens");
 
-} 
-module.exports= verifyUser
+const verifyUser = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(400).json({
+      success: false,
+      message: "Authorization header is missing. Please sign in.",
+    });
+  }
+  const token = authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(400).json({
+      success: false,
+      message: "Token is missing. Please sign in.",
+    });
+  }
+  try {
+    let user = await verifyJWT(token);
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid token. Please sign in.",
+      });
+    }
+    req.user = user.id;
+    next();
+  } catch (err) {
+    return res.status(400).json({
+      success: false,
+      message: "Tokken missing",
+    });
+  }
+};
+module.exports = verifyUser;
