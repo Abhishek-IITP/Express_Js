cec2c0e5a2dd81dfb57120b358ffcae044050869
Abhishek-IITP <abhishekmohanty7325@gmail.com>
2025-02-03 11:05:05 +0530

    new commit

--- BlogApp_users/Backend/Routes/blogRoutes.js
+++ BlogApp_users/Backend/Routes/blogRoutes.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const {createBlogs , getBlogs, getBlogsById, updateBlogs, deleteBlogs} = require('../controllers/blogController');
+const verifyUser = require('../Middlewares/auth');
 
 const route= express.Router();
 
-route.post("/blogs", createBlogs);
+route.post("/blogs", verifyUser ,createBlogs);
 
 route.get("/blogs",getBlogs );
 
 route.get("/blogs/:id",getBlogsById);
 
-route.patch("/blogs/:id",updateBlogs);
+route.patch("/blogs/:id",verifyUser,updateBlogs);
 
 route.delete("/blogs/:id", deleteBlogs);
 
