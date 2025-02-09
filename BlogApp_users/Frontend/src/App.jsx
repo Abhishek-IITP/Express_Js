2cf83a7da925a3e11f4f87868b8942ef2459126b
Abhishek-IITP <abhishekmohanty7325@gmail.com>
2025-02-09 11:10:10 +0530

    new commit

--- BlogApp_users/Frontend/src/App.jsx
+++ BlogApp_users/Frontend/src/App.jsx
@@ -1,65 +1,22 @@
-import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Signup from "./Pages/Signup";
+import Blogs from "./Components/Blogs";
+import SignIn from "./Pages/SignIn";
+import CreateBlogs from "./Components/CreateBlogs";
 
 function App() {
-  const [userData, setUserData] = useState({
-    name: "",
-    email: "",
-    password: "",
-  });
-
-  const [blogs, setBlogs]= useState([])
-async function fetchBlogs() {
-  let data= await fetch("http://localhost:3000/api/v1/blogs")
-  let res = await data.json()
-  console.log(res.blogs)
-  setBlogs(res.blogs);
-
-  
-}
-  useEffect(()=>{
-    fetchBlogs()
-  },[])
-  async function handleSubmit() {
-    let response = await fetch("http://localhost:3000/api/v1/users", {
-      method: "POST",
-      body: JSON.stringify(userData),
-
-      headers: {
-        "Content-Type": "application/json"
-      },
-    });
-    let res = await response.json();
-    alert(res.message)
-    console.log(res);
-  }
-  
-
   return (
-    <div>
-      <h1>signUp</h1>
-      <div>
-        <input onChange={(e)=>setUserData(prev =>({...prev,name:e.target.value}))} type="text" placeholder="Name" />
-        <br />
-        <br />
-        <input onChange={(e)=>setUserData(prev =>({...prev,email:e.target.value}))} type="email" placeholder="E-mail" />
-        <br />
-        <br />
-        <input onChange={(e)=>setUserData(prev =>({...prev,password:e.target.value}))} type="password" placeholder="Password" />
-      </div>
-      <br />
-      <button onClick={handleSubmit}>SignUp</button>
-
-      {
-        blogs.map(blog=>(
-          <ul>
-            <li>{blog.title}</li>
-            <p>{blog.description}</p>
-          </ul>
-
-        ))
-      }
-    </div>
+    <Routes>
+      <Route path="/" element={<Blogs />}></Route>
+      <Route path="/signup" element={<Signup />}></Route>
+      <Route path="/signin" element={<SignIn />}></Route>
+      <Route path="/Create-blogs" element={<CreateBlogs />}></Route>
+      <Route
+        path="*"
+        element={<h1>Kya baat h sheer tu idhar bhi aa gya</h1>}
+      ></Route>
+    </Routes>
   );
 }
 
