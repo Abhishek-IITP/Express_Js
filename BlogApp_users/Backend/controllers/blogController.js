29b14f71d856d37aec107f50cddcfc6bc6fe4a6c
Abhishek-IITP <abhishekmohanty7325@gmail.com>
2025-04-11 14:01:27 +0530

    CRUD operations in Blogs

--- BlogApp_users/Backend/controllers/blogController.js
+++ BlogApp_users/Backend/controllers/blogController.js
@@ -1,4 +1,5 @@
 const Blog = require("../models/blogSchema");
+const Comment = require("../models/commentSchema");
 const User = require("../models/userSchema");
 
 async function createBlogs(req, res) {
@@ -40,12 +41,20 @@ async function createBlogs(req, res) {
 async function getBlogs(req, res) {
   try {
     // const blogs = await Blog.find({ draft: false }).populate("creator");
-    const blogs = await Blog.find({ draft: false }).populate({
-      path: "creator",
-      select: "name",
-    });
+    const blogs = await Blog.find({ draft: false })
+      .populate({
+        path: "creator",
+        select: "-password",
+      })
+      .populate({
+        path: "likes",
+        select: "email  name",
+      });
 
-    return res.status(200).json({ blogs });
+    return res.status(200).json({
+      message: "Blogs Fetched Successfully",
+      blogs,
+    });
   } catch (err) {
     return res.status(500).json({
       message: "Error occurred while fetching blogs",
@@ -56,12 +65,19 @@ async function getBlogs(req, res) {
 
 async function getBlogsById(req, res) {
   try {
-    const { id } = req.params;
-    const blog = await Blog.findById({ draft: false });
-    return res.status(200).json({
-      message: "Blogs Fetched successfully",
-      blog,
+    const {id} = req.params;
+    const blog = await Blog.findById(id).populate({
+      path : "comments",
+      populate:{
+        path: 'user',
+        select: "name email"
+      }
     });
+    return res.status(200).json({
+      message: "Blog Fetched Successfully",
+      success: true,
+      blog
+    })
   } catch (err) {
     return res.status(500).json({
       message: "Error occurred while fetching blogs",
@@ -69,46 +85,84 @@ async function getBlogsById(req, res) {
     });
   }
 }
+
 async function updateBlogs(req, res) {
   try {
-  const creator = req.user;
-  const {title, description,draft}= req.body
-  const blogId= req.params;
-  const blog = await Blog.findById(blogId)
-  const user = await  User.findById(creator).select("-password")
+    const creator = req.user;
+    const { id } = req.params;
+    const { title, description, draft } = req.body;
 
-  // if (!(creator=== blog.creator)){
-  //   return res.status(404).json({
-  //     success:false,
-  //     message:"you are not authorized for this action"
-  //   })
-  // }
-    // const updatedBlog = await Blog.findByIdAndUpdate(blogId, {title,description,draft});
-    if (!updatedBlog) {
-      return res
-        .status(404)
-        .json({ message: "Blog not found", success: false });
+    // Find the user
+    const user = await User.findById(creator).select("-password");
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found.",
+        success: false,
+      });
     }
-    return res
-      .status(200)
-      .json({ message: "Blog updated successfully", updatedBlog });
-  } catch (err) {
+
+    // Find the blog
+    const blog = await Blog.findById(id);
+
+    if (!blog) {
+      return res.status(404).json({
+        message: "Blog not found.",
+        success: false,
+      });
+    }
+
+    // Check if the creator of the blog matches the user trying to update it
+    if (String(blog.creator) !== String(creator)) {
+      return res.status(403).json({
+        message: "You are not authorized to update this blog.",
+        success: false,
+      });
+    }
+
+    // Update only the fields that are provided in the request
+    if (title) blog.title = title;
+    if (description) blog.description = description;
+    if (draft !== undefined) blog.draft = draft;
+
+    // Save the updated blog
+    await blog.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Blog updated successfully",
+      blog,
+    });
+  } catch (error) {
     return res.status(500).json({
-      message: "Error occurred while updating blog",
-      error: err.message,
+      message: "Error occurred while updating the blog",
+      error: error.message,
     });
   }
 }
+
 async function deleteBlogs(req, res) {
-  const { id } = req.params;
   try {
-    const deletedBlog = await Blog.findByIdAndDelete(id);
-    if (!deletedBlog) {
-      return res
-        .status(404)
-        .json({ message: "Blog not found", success: false });
+    const { id } = req.params;
+    const creator = req.user;
+
+    const blog = await Blog.findById(id);
+    if (!blog) {
+      return res.status(500).json({
+        message: "Blog is not Found.",
+        success: false,
+      });
     }
-    return res.status(200).json({ message: "Blog deleted successfully" });
+    if (!(blog.creator == creator)) {
+      return res.status(403).json({
+        message: "You are not authorized to delete this blog.",
+        success: false,
+      });
+    }
+    await Blog.findByIdAndDelete(id);
+    await User.findByIdAndUpdate(creator, { $pull: { blogs: id } });
+    return res
+      .status(200)
+      .json({ success: true, message: "Blog deleted successfully" });
   } catch (err) {
     return res.status(500).json({
       message: "Error occurred while deleting blog",
@@ -117,10 +171,47 @@ async function deleteBlogs(req, res) {
   }
 }
 
+async function likeBlogs(req, res) {
+  try {
+    const { id } = req.params;
+    const creator = req.user;
+
+    const blog = await Blog.findById(id);
+    if (!blog) {
+      return res.status(500).json({
+        message: "Blog is not Found.",
+        success: false,
+      });
+    }
+    if (!blog.likes.includes(creator)) {
+      await Blog.findByIdAndUpdate(id, { $push: { likes: creator } });
+      return res.status(200).json({
+        success: true,
+        message: "Blog Liked successfully",
+      });
+    } else {
+      await Blog.findByIdAndUpdate(id, { $pull: { likes: creator } });
+
+      return res.status(400).json({
+        success: false,
+        message: "Blog disliked successfully",
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({
+      message: "Error occurred while Liking the blog blog",
+      error: err.message,
+    });
+  }
+}
+
+
+
 module.exports = {
   createBlogs,
   getBlogs,
-  getBlogsById,
+  getBlogsById, 
   updateBlogs,
   deleteBlogs,
+  likeBlogs,
 };
