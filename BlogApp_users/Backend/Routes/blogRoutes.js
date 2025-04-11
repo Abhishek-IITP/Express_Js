ce9241e2b2e658345a8383c5713d5493cd2c9547
Abhishek-IITP <abhishekmohanty7325@gmail.com>
2025-04-11 14:04:40 +0530

    set routes for likes and comment schema

--- BlogApp_users/Backend/Routes/blogRoutes.js
+++ BlogApp_users/Backend/Routes/blogRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const {createBlogs , getBlogs, getBlogsById, updateBlogs, deleteBlogs} = require('../controllers/blogController');
+const {createBlogs , getBlogs, getBlogsById, updateBlogs, deleteBlogs,likeBlogs} = require('../controllers/blogController');
 const verifyUser = require('../Middlewares/auth');
+const { addComment, deleteComment, editComment, likeComment } = require('../controllers/commentController');
 
 const route= express.Router();
 
@@ -12,8 +13,19 @@ route.get("/blogs/:id",getBlogsById);
 
 route.patch("/blogs/:id",verifyUser,updateBlogs);
 
-route.delete("/blogs/:id", deleteBlogs);
+route.delete("/blogs/:id",verifyUser, deleteBlogs);
 
+
+route.post("/blogs/like/:id",verifyUser, likeBlogs);
+
+route.post("/blogs/comment/:id",verifyUser, addComment);
+
+route.delete("/blogs/comment/:id",verifyUser, deleteComment);
+
+route.patch("/blogs/edit-comment/:id",verifyUser, editComment);
+
+
+route.patch("/blogs/like-comment/:id",verifyUser, likeComment);
 
 
 module.exports=route
