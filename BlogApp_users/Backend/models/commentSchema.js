6c98c88169ccb43536692cbf49853e3dfb636b9f
Abhishek-IITP <abhishekmohanty7325@gmail.com>
2025-04-11 14:03:44 +0530

    create comment schema

---
+++ BlogApp_users/Backend/models/commentSchema.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const commentSchema = new mongoose.Schema(
+  {
+    comment: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    blog: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Blog",
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+module.exports = Comment;
