89794ffadd8c6ffa9fe18d5ed1c4c04290913da9
Abhishek-IITP <abhishekmohanty7325@gmail.com>
2025-04-12 12:38:53 +0530

    Update server.js

--- BlogApp_users/Backend/server.js
+++ BlogApp_users/Backend/server.js
@@ -5,7 +5,8 @@ const app = express();
 const userRoutes= require("./Routes/userRoutes")
 const blogRoutes= require("./Routes/blogRoutes")
 const mongoose = require("mongoose");
-const User=require("./models/userSchema")
+const User=require("./models/userSchema");
+const cloudinaryConfig = require("./Config/cloudinaryConfig");
 
 app.use(cors());
 app.use(express.json());
@@ -17,4 +18,5 @@ app.use("/api/v1",blogRoutes)
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
   dbConnect();
+  cloudinaryConfig()
 });
